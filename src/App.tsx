92b2ba50fe34b0956e488af8dd694f9bfd79e00e
김진호 <rlawlsgh1227@gmail.com>
2024-07-15 20:48:06 +0900

    hsdfljskdf
    Co-authored-by: pakxe <pigkill40@naver.com>
     Co-authored-by: soi-ha <soy2302ten@gmail.com>
     Co-authored-by: jinhokim98 <rlawlsgh1227@gmail.com>
     Co-authored-by: Todari <rhymint@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import { divStyle } from './App.style';
 
 const App = () => {
-  return <div css={divStyle}>👑행동대장 안녕</div>;
+  return <div css={divStyle}>👑행동대장 안녕하세유</div>;
 };
 
 export default App;
